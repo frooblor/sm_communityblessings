ef15689cb860445a5f56ef16ccddaf2d86fe6a1d
rouxpz <roopakutty@gmail.com>
2012-03-03 15:13:47 -0500

    Update arduino_spatialmedia/arduino_spatialmedia.ino

--- arduino_spatialmedia/arduino_spatialmedia.ino
+++ arduino_spatialmedia/arduino_spatialmedia.ino
@@ -1,7 +1,17 @@
-int receiverPin1 = 8; //input from IR receiver
-int receiverPin2 = 9;
-int receiverPin3 = 10;
-//int ledPin = 8;
+int lightingPin1 = 10; //input from IR receiver
+int lightingPin2 = 11;
+int lightingPin3 = 12;
+int lightingPin4 = 13;
+
+int votivePin1 = 1;
+int votivePin2 = 2;
+int votivePin3 = 3;
+int votivePin4 = 4;
+int votivePin5 = 5;
+int votivePin6 = 6;
+int votivePin7 = 7;
+int votivePin8 = 8;
+
 //int counter;
 
 //unsigned long duration; //time beam is broken
@@ -9,28 +19,51 @@ int receiverPin3 = 10;
 void setup () {
   
   Serial.begin(9600);
-  pinMode(receiverPin1, INPUT);
-  pinMode(receiverPin2, INPUT);
-  pinMode(receiverPin3, INPUT);
-  //pinMode(ledPin, OUTPUT);
+  pinMode(votivePin1, INPUT);
+  pinMode(votivePin2, INPUT);
+  pinMode(votivePin3, INPUT);
+  pinMode(votivePin4, INPUT);
+  pinMode(votivePin5, INPUT);
+  pinMode(votivePin6, INPUT);
+  pinMode(votivePin7, INPUT);
+  pinMode(votivePin8, INPUT);
+  pinMode(lightingPin1, INPUT);
+  pinMode(lightingPin2, INPUT);
+  pinMode(lightingPin3, INPUT);
+  pinMode(lightingPin4, INPUT);
+
+//pinMode(ledPin, OUTPUT);
   //counter = 0;
   
 }
 
 void loop () {
-/*  if (digitalRead(receiverPin1) == HIGH && digitalRead(receiverPin2) == LOW) {
-    digitalWrite(ledPin, HIGH);
-  } else if (digitalRead(receiverPin1) == LOW && digitalRead(receiverPin2) == HIGH) {
-    digitalWrite(ledPin, HIGH);
-  } else {
-    digitalWrite(ledPin, LOW);
-  }*/
   
-  Serial.print(digitalRead(receiverPin1));
+  Serial.print(digitalRead(lightingPin1));
   Serial.print(",");
-  Serial.print(digitalRead(receiverPin2));
+  Serial.print(digitalRead(lightingPin2));
   Serial.print(",");
-  Serial.println(digitalRead(receiverPin3));
+  Serial.print(digitalRead(lightingPin3));
+  Serial.print(",");
+  Serial.print(digitalRead(lightingPin4));
+  Serial.print(",");
+  
+  Serial.print(digitalRead(votivePin1));
+  Serial.print(",");
+  Serial.print(digitalRead(votivePin2));
+  Serial.print(",");
+  Serial.print(digitalRead(votivePin3));
+  Serial.print(",");
+  Serial.print(digitalRead(votivePin4));
+  Serial.print(",");
+  Serial.print(digitalRead(votivePin5));
+  Serial.print(",");
+  Serial.print(digitalRead(votivePin6));
+  Serial.print(",");
+  Serial.print(digitalRead(votivePin7));
+  Serial.print(",");
+  Serial.print(digitalRead(votivePin8));
+  Serial.println(",");
   
   
 }
